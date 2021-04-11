63ce85b99b62ae4edb3165aabab5ffcafb78d773
Kibet 1998 <ronoronald1998@gmail.com>
2021-04-11 21:49:32 +0300

    add README.md

--- Js/script.js
+++ Js/script.js
@@ -4,6 +4,8 @@ function submitFunction(){
     var inputYear = checkYear();
     var inputGender = checkGender();
     var calculateDay = findDayOfWeek(inputDate,inputMonth,inputYear);
+    var contain = determineName(inputGender,calculateDay);
+    alert("Your AKAN name is " + contain );
 
 
 
@@ -11,7 +13,6 @@ function submitFunction(){
 
 function checkDate(){
     var date = document.getElementById("date").value;
-    console.log(date);
     if (date) {
         if (date>=1 && date<=31) {
             return date;
@@ -31,7 +32,6 @@ function checkDate(){
 
     function checkMonth(){
         var month = document.getElementById("month").value;
-        console.log(month);
         if (month) {
             if (month>=1 && month<=12) {
                 return month;
@@ -47,7 +47,6 @@ function checkDate(){
 
         function checkYear(){
             var year = document.getElementById("year").value;
-            console.log(year);
             if (year) {
                 if (year>=1000 && year<=9999) {
                     return year;
@@ -63,7 +62,6 @@ function checkDate(){
 
             function checkGender(){
                 var gender = document.getElementById("pickGender").value;
-                console.log(gender);
                 if (gender) {
                     if (gender=="male" || gender=="female") {
                         return gender;
@@ -81,8 +79,8 @@ function checkDate(){
                     var DD = day;
                     var MM = month;
                     var YY = parseInt(String(year).slice(2,4));
-                    var cc = parseInt(String(year).slice(0,2));
-                    var DayOfWeek = Math.round(  ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
+                    var CC = parseInt(String(year).slice(0,2));
+                    var DayOfWeek = Math.round( (  ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7);
                     var dayPerOrder = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
                     var orderOfDay = DayOfWeek-1;
                     var nameOfDay = dayPerOrder[orderOfDay];
