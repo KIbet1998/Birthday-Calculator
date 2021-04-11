d803816b24fdc1de70fd42c60c69f205fc0f67d8
Kibet 1998 <ronoronald1998@gmail.com>
2021-04-11 16:45:50 +0300

    add date function

--- Js/script.js
+++ Js/script.js
@@ -1,8 +1,48 @@
 function submitFunction(){
+    checkDate();
+    // console.log(date);
 
 
 }
 
 function checkDate(){
-    var date = document.getElementById("")
-}
+    var date = document.getElementById("date").value;
+    console.log(date);
+    if (date) {
+        if (date>=1 && date<=31) {
+            return date;
+        }
+        else{
+            alert("date should be between 1-31");
+        }
+    }else{
+        alert("enter a valid date")
+
+    }
+    
+
+        
+        
+    }
+
+    function checkDate(){
+        var date = document.getElementById("date").value;
+        console.log(date);
+        if (date) {
+            if (date>=1 && date<=31) {
+                return date;
+            }
+            else{
+                alert("date should be between 1-31");
+            }
+        }else{
+            alert("enter a valid date")
+    
+        }
+        
+    
+            
+            
+        }
+
+        
